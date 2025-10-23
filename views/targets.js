085d5769dd77d46b3c4d3e65864512ad52b855cc
veliok <veli.okkonen@gmail.com>
2025-10-23 15:48:47 +0300

    v1.0

---
+++ views/targets.js
@@ -0,0 +1,30 @@
+import { getTargetData } from "../main.js";
+
+export function renderTargets(container) {
+  const section = document.createElement("div");
+  section.className = "content";
+
+  const title = document.createElement("h1");
+  const grid = document.createElement("div");
+  title.textContent = "Henkilöt";
+  grid.id = "button-grid";
+
+  const targetData = getTargetData();
+
+  targetData.forEach(target => {
+    if (target.name.length > 1) {
+      const targetButton = document.createElement("button");
+      targetButton.textContent = target.name;
+
+      targetButton.addEventListener("click", () => {
+        window.location.hash = `#target/${target.id[0]}`;
+      });
+
+      grid.appendChild(targetButton);
+    }
+  })
+
+  section.appendChild(title);
+  section.appendChild(grid);
+  container.appendChild(section);
+}
